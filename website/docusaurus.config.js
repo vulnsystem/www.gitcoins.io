c2f4f3be629711ff00b80b0b916f4923229fd934
thebestornothing <gongzhe@live.com>
2021-06-06 23:26:22 +0800

    config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,7 +38,6 @@ module.exports = {
           editUrl:
             'https://github.com/vulnsystem/www.gitcoins.io/blob/documentation/website/',
           path: '../docs',
-          routeBasePath: '../docs',
           sidebarPath: require.resolve('./sidebars.json'),
           remarkPlugins: [require('@react-native-website/remark-snackplayer')],
           editCurrentVersion: true,
